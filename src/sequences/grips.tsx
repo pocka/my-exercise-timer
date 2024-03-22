5e8258c8f383fc95a57e242eecf3b183ced9e062
Shota FUJI <pockawoooh@gmail.com>
2024-03-22 21:06:20 +0900

    Update grips menu

--- src/sequences/grips.tsx
+++ src/sequences/grips.tsx
@@ -1,8 +1,6 @@
 import { type SequenceComposerController } from "../components/SequenceComposer";
 import { Action, Countdown, Description, Layout, Title } from "../components/SingleActionScreen";
 
-import { rest } from "./rest";
-
 const SPINNER_DURATION_SECONDS = 60;
 
 export function* grips(sequence: SequenceComposerController, sets: number = 3) {
@@ -10,7 +8,7 @@ export function* grips(sequence: SequenceComposerController, sets: number = 3) {
 		<Layout>
 			<Title speak>Warm-up Gripper</Title>
 			<Description>
-				Close your warm-up gripper,	 10-12 reps.
+				Close your warm-up gripper,	 10-12 reps each hand.
 			</Description>
 			<Action
 				onClick={() => {
@@ -22,7 +20,17 @@ export function* grips(sequence: SequenceComposerController, sets: number = 3) {
 		</Layout>
 	);
 
-	yield* rest(sequence, 30);
+	yield (
+		<Layout>
+			<Title speak>
+				Extensor Bands
+			</Title>
+			<Description>
+				Lightly expand bands using your fingers.
+			</Description>
+			<Countdown seconds={30} onCountdownEnd={sequence.next} />
+		</Layout>
+	);
 
 	for (let i = 0; i < sets; i++) {
 		yield (
@@ -31,7 +39,7 @@ export function* grips(sequence: SequenceComposerController, sets: number = 3) {
 					Working Gripper ({i + 1}/{sets})
 				</Title>
 				<Description>
-					Close your working gripper, 5-7 reps.
+					Close your working gripper, 5-7 reps each hand.
 				</Description>
 				<Action
 					onClick={() => {
@@ -43,58 +51,24 @@ export function* grips(sequence: SequenceComposerController, sets: number = 3) {
 			</Layout>
 		);
 
-		yield* rest(sequence);
+		yield (
+			<Layout>
+				<Title speak>
+					Extensor Bands
+				</Title>
+				<Description>
+					Lightly expand bands using your fingers.
+				</Description>
+				<Countdown seconds={30} onCountdownEnd={sequence.next} />
+			</Layout>
+		);
 	}
-
-	yield (
-		<Layout>
-			<Title speak>Extensor Bands</Title>
-			<Description>
-				Expand your hand using bands, 10-20 reps.
-			</Description>
-			<Action
-				onClick={() => {
-					sequence.next();
-				}}
-			>
-				Next
-			</Action>
-		</Layout>
-	);
-
-	yield (
-		<Layout>
-			<Title speak>NSD Spinner (Left)</Title>
-			<Description speak>
-				Press the button when it reaches enough RPM.
-			</Description>
-			<Action
-				onClick={() => {
-					sequence.next();
-				}}
-			>
-				Next
-			</Action>
-		</Layout>
-	);
-
-	yield (
-		<Layout>
-			<Title>
-				NSD Spinner (Left)
-			</Title>
-			<Description speak>
-				Keep the RPM for {SPINNER_DURATION_SECONDS} seconds.
-			</Description>
-			<Countdown seconds={SPINNER_DURATION_SECONDS} onCountdownEnd={sequence.next} />
-		</Layout>
-	);
 
 	yield (
 		<Layout>
 			<Title speak>NSD Spinner (Right)</Title>
 			<Description speak>
-				Press the button when it reaches enough RPM.
+				Press the button when it reaches target RPM.
 			</Description>
 			<Action
 				onClick={() => {
@@ -110,6 +84,34 @@ export function* grips(sequence: SequenceComposerController, sets: number = 3) {
 		<Layout>
 			<Title>
 				NSD Spinner (Right)
+			</Title>
+			<Description speak>
+				Keep the RPM for {SPINNER_DURATION_SECONDS} seconds.
+			</Description>
+			<Countdown seconds={SPINNER_DURATION_SECONDS} onCountdownEnd={sequence.next} />
+		</Layout>
+	);
+
+	yield (
+		<Layout>
+			<Title speak>NSD Spinner (Left)</Title>
+			<Description speak>
+				Press the button when it reaches target RPM.
+			</Description>
+			<Action
+				onClick={() => {
+					sequence.next();
+				}}
+			>
+				Next
+			</Action>
+		</Layout>
+	);
+
+	yield (
+		<Layout>
+			<Title>
+				NSD Spinner (Left)
 			</Title>
 			<Description speak>
 				Keep the RPM for {SPINNER_DURATION_SECONDS} seconds.
